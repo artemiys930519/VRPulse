9339d0e5df8e2cb58c5b6bd178342747f4737d7a
Artem <artemiys930519@yandex.ru>
2023-06-05 09:40:29 +0300

    set common installer

---
+++ Assets/Scripts/Installers/CommonInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Shared;
+using Zenject;
+
+namespace Installers
+{
+    public class CommonInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<IServerRepository>().To<ServerRepository>().AsSingle();
+
+        }
+    }
+}
